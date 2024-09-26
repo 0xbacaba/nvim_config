0cb7ac58caa3ef7b211a833e747032075e861367
Joshi <joshua@lipps.de>
2024-09-26 16:08:12 +0200

    [clangd] add very basic debugging for c and c++

--- lua/lsp/clangd.lua
+++ lua/lsp/clangd.lua
@@ -42,6 +42,28 @@ end
 return {
   setup = function(default_config)
     local lspconfig = require "lspconfig"
+    local dap = require "dap"
+
+    local codelldb_path = require("mason-registry").get_package("codelldb"):get_install_path()
+    dap.adapters.codelldb = {
+      type = "server",
+      port = "${port}",
+      executable = {
+        command = codelldb_path .. "/extension/adapter/codelldb",
+        args = { "--port", "${port}" },
+      },
+    }
+    dap.configurations.cpp = {
+      {
+        name = "Launch file",
+        type = "codelldb",
+        request = "launch",
+        program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+      },
+    }
+    dap.configurations.c = dap.configurations.cpp
 
     local config = vim.tbl_deep_extend("force", default_config, {
       cmd = { "clangd" },
