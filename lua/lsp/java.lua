6ebedc50881442db326e18b51ffab2e722dc618d
Joshi <joshua@lipps.de>
2024-09-26 16:00:58 +0200

    [jdtls] fix java config related 'table.unpack == nil' error

--- lua/lsp/java.lua
+++ lua/lsp/java.lua
@@ -161,7 +161,7 @@ return {
         debounce_text_changes = 80,
       },
       on_attach = on_attach, -- We pass our on_attach keybindings to the configuration map
-      root_dir = lspconfig.util.root_pattern(table.unpack(root_markers)), -- Set the root directory to our found root_marker
+      root_dir = lspconfig.util.root_pattern(table.unpack and table.unpack(root_markers)), -- Set the root directory to our found root_marker
       init_options = {
         bundles = bundles,
       },
