3bc50ab65dbd4bd63980289da32a071a772b6b86
Joshi <joshua@lipps.de>
2024-11-07 17:28:41 +0100

    get rid of neo_test error on first startup

--- lua/plugins/neo_test.lua
+++ lua/plugins/neo_test.lua
@@ -1,10 +1,12 @@
 return {
   {
     "nvim-neotest/neotest",
+    lazy = true,
     dependencies = {
       "nvim-lua/plenary.nvim",
       "antoinemadec/FixCursorHold.nvim",
       "alfaix/neotest-gtest",
+      "nvim-treesitter/nvim-treesitter",
     },
     config = function()
       require("neotest").setup {
