b94516c7bd00d20b6bcdcc8b6948d2b4ed058482
Joshi <joshua@lipps.de>
2024-11-14 14:43:47 +0100

    [python] add keybind to install a package with pip

--- lua/lsp/python.lua
+++ lua/lsp/python.lua
@@ -1,6 +1,21 @@
-return {
+local M
+M = {
   setup = function(default_config)
     local config = vim.tbl_deep_extend("force", default_config, {
+      on_attach = function(client, bufnr)
+        local utils = require "utils"
+
+        utils.set_lsp_keybinds(client, bufnr)
+
+        utils.set_keybinds({
+          {
+            "Install package with pip",
+            "<leader>lp",
+            utils.mapmode.normal,
+            M.pip_install,
+          },
+        }, bufnr)
+      end,
       settings = {
         pylsp = {
           plugins = {
@@ -15,4 +30,20 @@ return {
 
     require("lspconfig").pylsp.setup(config)
   end,
+  pip_install = function(package)
+    local pylsp_install = require("mason-registry").get_package("python-lsp-server"):get_install_path()
+    local utils = require "utils"
+    local pylsp_python = pylsp_install .. "/venv/bin/python3"
+
+    utils.ask_to_run(pylsp_python .. " -m pip install ", function(install_success)
+      if install_success then
+        vim.notify "Installation successful"
+        return
+      end
+
+      vim.notify("Installation failed", vim.log.levels.ERROR)
+    end)
+  end,
 }
+
+return M
