5364bf3bd1b33a6246bab353b3846de5766dc791
Joshi <joshua@lipps.de>
2024-11-14 14:33:27 +0100

    add run function to utils and use in ask_to_run

--- lua/utils.lua
+++ lua/utils.lua
@@ -2,19 +2,27 @@ local keybinds = require "utils.keybinds"
 local data = require "utils.data"
 local color = require "utils.color"
 
-return vim.tbl_deep_extend("force", keybinds, data, color, {
-  ask_to_run = function(command, callback)
-    local cmd = vim.fn.input("Run command: ", command)
-    if cmd == "" then return end
-
+local M
+M = vim.tbl_deep_extend("force", keybinds, data, color, {
+  run = function(command, args, callback)
     local handle
-    handle = vim.uv.spawn("sh", {
-      args = { "-c", cmd },
+    handle = vim.uv.spawn(command, {
+      args = args,
       stdio = { nil, vim.uv.new_pipe(false), vim.uv.new_pipe(false) },
     }, function(code, _)
       if handle ~= nil then handle:close() end
 
-      if callback ~= nil then vim.schedule(function() callback(code == 0) end) end
+      if callback ~= nil then vim.schedule(function() callback(code) end) end
+    end)
+  end,
+  ask_to_run = function(command, callback)
+    local cmd = vim.fn.input("Run command: ", command)
+    if cmd == "" then return end
+
+    M.run("sh", { "-c", cmd }, function(code)
+      if callback ~= nil then callback(code == 0) end
     end)
   end,
 })
+
+return M
