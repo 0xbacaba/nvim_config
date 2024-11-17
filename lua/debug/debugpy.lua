4c045812d83a0231cc646eed6f8712066c4b6317
Joshi <joshua@lipps.de>
2024-11-18 00:24:11 +0100

    [python] adjust path mappings for debug config

--- lua/debug/debugpy.lua
+++ lua/debug/debugpy.lua
@@ -26,5 +26,38 @@ return {
     end
 
     require("dap-python").setup(pylsp_python)
+
+    local dap = require "dap"
+    local configs = dap.configurations
+
+    -- Replace "Attach remote" python debug configuration
+    local remote_index = -1
+    for i, config in ipairs(configs.python) do
+      if config.name == "Attach remote" then
+        remote_index = i
+        break
+      end
+    end
+    if remote_index == -1 then
+      vim.notify("[debugpy] 'Attach remote' config not found", vim.log.levels.WARN)
+      return
+    end
+
+    configs.python[remote_index] = {
+      type = "python",
+      request = "attach",
+      name = "Attach remote",
+      connect = function()
+        local host = vim.fn.input "Host [127.0.0.1]: "
+        host = host ~= "" and host or "127.0.0.1"
+
+        local port = tonumber(vim.fn.input "Port [5678]: ") or 5678
+
+        return { host = host, port = port }
+      end,
+      pathMappings = {
+        { localRoot = "${workspaceFolder}", remoteRoot = "." },
+      },
+    }
   end,
 }
